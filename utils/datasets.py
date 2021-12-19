55fdd90d0079d5cececc3533b05350e40e56b4ed
minari <hieutt99@gmail.com>
2021-12-20 00:46:41 +0700

    f

--- utils/datasets.py
+++ utils/datasets.py
@@ -871,8 +871,8 @@ def verify_image_label(args):
     nm, nf, ne, nc, msg, segments = 0, 0, 0, 0, '', []  # number (missing, found, empty, corrupt), message, segments
     try:
         # verify images
-        im = Image.open(im_file)
         print(f"vao day {im_file} {lb_file}")
+        im = Image.open(im_file)
         im.verify()  # PIL verify
         shape = exif_size(im)  # image size
         assert (shape[0] > 9) & (shape[1] > 9), f'image size {shape} <10 pixels'
